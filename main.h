ad66d187d6b186f8f920a65ab7cfeb92904e3206
marcobrembi <marco.brembi@gmail.com>
2025-11-27 20:14:46 +0100

    end

--- main.h
+++ main.h
@@ -1,22 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Legge 1 byte sicuro da /dev/urandom (tramite FILE* aperto in main) */
 int get_file_noise(FILE *fp);
-
-/* Genera un ordine casuale dei 4 blocchi (1..4) */
 void d4(int *order, FILE *fp);
-
-/* Genera 2 caratteri speciali + terminatore */
 void d8(char *speciali, FILE *fp);
-
-/* Genera 3 cifre numeriche + terminatore */
 void d10(char *numbers, FILE *fp);
-
-/* Genera 7 lettere alfabetiche (maiuscole/minuscole) + terminatore */
 void d20_d12_d6(unsigned char *ascii, FILE *fp);
-
-#endif
